60e960e446f6862c68847e48bd9835d4afd55e1b
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-11-07 17:43:42 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context2) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the Second page'),
+          onPressed: () {
+            Navigator.push(
+                context2,
+                MaterialPageRoute(
+                  // 둘 다 같은 결과
+                  // builder: (BuildContext context){
+                  //   return SecondPage();
+                  // }
+                    builder: (_) => SecondPage()));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the First page'),
+          onPressed: () {
+            Navigator.pop(ctx);
+          },
+        ),
+      ),
+    );
+  }
+}
